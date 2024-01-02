ae3d6bf052af3382f4ca6ff65648d30ddfa18ad1
a114383 <A114383@progressive.com>
2024-01-02 09:05:49 -0500

    day 18

--- 2023/18/main.go
+++ 2023/18/main.go
@@ -13,41 +13,51 @@ type Digger struct {
 	horizontal int
 }
 
-func (d Digger) dig(lagoon [][]int, direction string, depth int) {
-	for i := 0; i <= depth; i++ {
-		switch direction {
-		case "U":
-			d.vertical++
-		case "D":
-			d.vertical--
-		case "L":
-			d.horizontal--
-		case "R":
-			d.horizontal++
-		}
+func (digger *Digger) Move(direction string, depth int) {
+	switch direction {
+	case "U":
+		digger.vertical += depth
+	case "D":
+		digger.vertical -= depth
+	case "L":
+		digger.horizontal -= depth
+	case "R":
+		digger.horizontal += depth
 	}
-	lagoon[d.vertical][d.vertical] = 1
 }
 
 func p1ProcessLines(lines []string) (int, error) {
-	lagoon := make([][]int, 1000)
-	for i := range lagoon {
-		lagoon[i] = make([]int, 1000)
-	}
-
-	// picking randomly?
-	digger := Digger{vertical: 500, horizontal: 500}
-	lagoon[500][500] = 1
+	area := 0
+	steps := 1
+	digger := &Digger{vertical: 0, horizontal: 0}
 
 	for _, line := range lines {
 		splitLines := strings.Split(line, " ")
 		direction, depth, _ := splitLines[0], splitLines[1], splitLines[2]
-		iDepth, _ := strconv.Atoi(depth)
-		digger.dig(lagoon, direction, iDepth)
-		fmt.Printf("Lagoon:\n%v\n", lagoon)
+		iDepth, err := strconv.Atoi(depth)
+		if err != nil {
+			return 0, err
+		}
+
+		previousLocation := &Digger{vertical: digger.vertical, horizontal: digger.horizontal}
+		// fmt.Printf("Previous Location (%d, %d)\n", previousLocation.vertical, previousLocation.horizontal)
+
+		digger.Move(direction, iDepth)
+		// fmt.Printf("Location (%d, %d)\n", digger.vertical, digger.horizontal)
+
+		area += (previousLocation.vertical*digger.horizontal - previousLocation.horizontal*digger.vertical) + iDepth
+		// fmt.Printf("Area: %d\n", area)
+		steps += 1
 	}
 
-	return 405, nil
+	// This took me a while to figure out, but this was really helpful: https://advent-of-code.xavd.id/writeups/2023/day/18/
+	// The key here is to realize that the "area" of the shape after shoelace isn't including the space of the perimeter.
+	// return (area / 2) + (steps / 2) - 1, nil
+
+	// No idea why this works with changes on line 48
+	// https://github.com/bsadia/aoc_goLang/blob/53ed198e644324d559a366a17c712e1b8c6bb4fe/day18/main.go
+	// Note: Shoelace is only for 2D space.  So we're not finding area - We're finding VOLUME
+	return (area / 2) + 1, nil
 }
 
 func p2ProcessLines(lines []string) (int, error) {
@@ -55,7 +65,7 @@ func p2ProcessLines(lines []string) (int, error) {
 }
 
 func main() {
-	lines := aoc.ReadLines("")
+	lines := aoc.ReadLines("input.txt")
 
 	sum, err := p1ProcessLines(lines)
 	if err != nil {
