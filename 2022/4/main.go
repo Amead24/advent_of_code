0f6e3d026d13f76d7043ae8865b64a98ef89f80a
a114383 <A114383@progressive.com>
2024-01-22 10:31:32 -0500

    day 4 - overthought it way to much

--- 2022/4/main.go
+++ 2022/4/main.go
@@ -2,16 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 
 	"github.com/amead24/advent_of_code/aoc"
 )
 
 func p1ProcessLines(lines []string) (int, error) {
-	return 0, nil
+	overlap := 0
+	for _, line := range lines {
+		reSections := regexp.MustCompile("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)")
+		sections := reSections.FindStringSubmatch(line)
+		if len(sections) != 5 {
+			log.Fatalf("Expected 5, got %d, line: %v\n", len(sections), line)
+		}
+
+		numbers := aoc.MapInt(sections[1:])
+
+		if (numbers[0] <= numbers[2]) && (numbers[1] >= numbers[3]) ||
+			(numbers[2] <= numbers[0]) && (numbers[3] >= numbers[1]) {
+			// slog.Warn(fmt.Sprintf("Overlap: %v\n", line))
+			overlap++
+		}
+	}
+	return overlap, nil
 }
 
 func p2ProcessLines(lines []string) (int, error) {
-	return 0, nil
+	overlap := 0
+	for _, line := range lines {
+		reSections := regexp.MustCompile("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)")
+		sections := reSections.FindStringSubmatch(line)
+		if len(sections) != 5 {
+			log.Fatalf("Expected 5, got %d, line: %v\n", len(sections), line)
+		}
+
+		numbers := aoc.MapInt(sections[1:])
+		if (numbers[1] >= numbers[2]) && (numbers[3] >= numbers[0]) {
+			// slog.Warn(fmt.Sprintf("Overlap: %v\n", line))
+			overlap++
+		}
+	}
+	return overlap, nil
 }
 
 func main() {
--- 2022/4/main_test.go
+++ 2022/4/main_test.go
@@ -4,29 +4,17 @@ import (
 	"testing"
 )
 
-func TestFunc(t *testing.T) {
-		testCases := []struct {
-		input string
-		want  int
-	}{
-		{"rn=1", 30},
-		{"cm-", 253},
-		{"qp=3", 97},
-		{"rn", 0},
-		{"cm", 0},
-	}
-
-	// Iterate over the test cases
-	for _, tc := range testCases {
-		got := hash(tc.input)
-		if got != tc.want {
-			t.Errorf("Hash(%q) = %d; want %d", tc.input, got, tc.want)
-		}
-	}
-}
-
 func TestP1ProcessLines(t *testing.T) {
-	want := 405
+	testRows := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8",
+	}
+
+	want := 2
 	got, err := p1ProcessLines(testRows)
 	if err != nil {
 		t.Errorf("processLines() error = %v", err)
@@ -38,9 +26,18 @@ func TestP1ProcessLines(t *testing.T) {
 }
 
 func TestP2ProcessLines(t *testing.T) {
-	testRows := []string{}
+	testRows := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8",
+		"4-6,2-3",
+		"1-9,0-0",
+	}
 
-	want := 46
+	want := 4
 	got, err := p2ProcessLines(testRows)
 	if err != nil {
 		t.Errorf("processLines() error = %v", err)
